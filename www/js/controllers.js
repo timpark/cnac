913c60720066e90751a9a169642be5824ce96b6e
Tim Park <tim@arcticempire.ca>
2015-06-06 10:44:14 -0400

    First pass at simple mockup

---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('ExperienceCtrl', function($scope) {})
+
+.controller('BenefitsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('RiskCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('ControlCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
